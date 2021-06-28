bbd5d9b0ae1b7717505f552251de2bccc3ec0c17
beauwilliams <beau.williams@icloud.com>
2021-06-28 14:00:13 +1000

    added get at fn to doublell. taking a break :)

--- DataStructures/Typescript/linked_list/src/doublylinked.ts
+++ DataStructures/Typescript/linked_list/src/doublylinked.ts
@@ -9,12 +9,13 @@ export interface IDoublyLinked<T> {
     dequeue(): T | null; // O(1)
     push(data: T): void; // O(1)
     pop(): T | null; // O(1)
-    getAt(index: number): T | null; // O(n)
+    getAt(index: number): DataNode<T> | null; // O(n)
     insertAt(index: number, data: T): void; // O(n)
     removeAt(index: number): T | null; // O(n)
     peekHead(): T | null; // O(1)
     peekTail(): T | null; // O(1)
     size(): number; //O(1)
+    cur: number;
 }
 
 export class DoublyLinked<T> implements IDoublyLinked<T> {
@@ -119,9 +120,43 @@ export class DoublyLinked<T> implements IDoublyLinked<T> {
         return this.tail.data;
     }
 
-    getAt(index: number): void { }
+    getAt(index: number): DataNode<T> | null { //index starts from head.
+        let curNode: DataNode<T> | null;
 
-    insertAt() { }
+        if (index == 0) { //O(1)
+            return this.head;
+        }
+
+        if (index === this._size) { //O(1)
+            return this.tail;
+        }
+
+        if (index < this._size / 2) {
+            curNode = this.head;
+            for (let i = 0; i < index; ++i) {
+                if (curNode !== null) {
+                    curNode = curNode.next;
+                }
+            }
+
+        } else {
+            curNode = this.tail;
+            for (let i = 0; i > index; --i) {
+                if (curNode !== null) {
+                    curNode = curNode.prev;
+                }
+            }
+
+        }
+        return curNode;
+    }
+
+    insertAt(index: number, data: T): void {
+        let node = this.getAt(index)
+
+
+
+    }
 
     removeAt() { }
 
